c0697789b3546fb9e4eec6abde2b95728bdc1091
asahiro <asahiro.g@gmail.com>
2017-09-13 08:15:26 +0900

    escape question & ampersand

--- Formula/fltk.rb
+++ Formula/fltk.rb
@@ -13,7 +13,7 @@ class Fltk < Formula
 	end
 
 	devel do
-		url "http://www.fltk.org/software.php?VERSION=1.4.x-r12411&FILE=fltk/snapshots/fltk-1.4.x-r12431.tar.gz"
+		url "http://www.fltk.org/software.php\?VERSION=1.4.x-r12411\&FILE=fltk/snapshots/fltk-1.4.x-r12431.tar.gz"
 		sha256 "ae70fcb7293385227462b8af1e1334fad87e3134d6666b17f42a3b16a1218c81"
 	end
 
